a88254e7547b456ffbb1be4afed06b62380b08e1
Kim-ilhyeon <kih3915@naver.com>
2025-06-10 01:30:38 +0900

    revert MemberController.java

--- src/main/java/kr/co/khedu/fitroutine/member/controller/MemberController.java
+++ src/main/java/kr/co/khedu/fitroutine/member/controller/MemberController.java
@@ -21,7 +21,6 @@ public final class MemberController {
 
         return ResponseEntity.ok(memberService.getMemberProfile(memberId));
     }
-    
     @PutMapping("/me")
     public ResponseEntity<?> editMemberProfile(@RequestBody MemberEditInfo memberEditInfo) {
         // 추후에 토큰에서 회원을 얻도록 변경해야 합니다.
